06c434b307941b5742ac281cafc748ef72498f2b
wguyman <wguyman@gmail.com>
2012-10-25 20:17:04 -0700

    Removed unnecessary comments

--- d3Bubble.js
+++ d3Bubble.js
@@ -4,24 +4,13 @@ var h = 600;
 var padding = 30;
 
 
-//Static dataset [Year, population, age, ratio of deaths to births]
+//Static dataset in the format [Year, population, age, ratio of deaths to births]
 var dataset = [
 				[1990, 500, 40, 5], [1991, 600, 45, 6], [1992, 700, 50, 6], [1993, 800, 50, 6], [1994, 850, 95, 6],
 				[1995, 900, 70, 7], [1996, 1000, 65, 7], [1997, 1100, 67, 5], [1998, 1200, 75, 8], [1999, 1300, 88, 6],
 				[2000, 1300, 85, 8]
 			  ];
 
-
-// //Dynamic, random dataset
-// var dataset = [];					//Initialize empty array
-// var numDataPoints = 10;				//Number of dummy data points to create
-// var xRange = Math.random() * 1000;	//Max range of new x values
-// var yRange = Math.random() * 1000;	//Max range of new y values
-// for (var i = 0; i < numDataPoints; i++) {					//Loop numDataPoints times
-// 	var newNumber1 = Math.round(Math.random() * xRange);	//New random integer
-// 	var newNumber2 = Math.round(Math.random() * yRange);	//New random integer
-// 	dataset.push([newNumber1, newNumber2]);					//Add new number to array
-// }
 
 //Create scale functions
 var xScale = d3.scale.linear()
