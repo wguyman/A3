3ec7ee56750a7ddfd812efd00a4ce903bc48906d
stephanieguo <stguo@stanford.edu>
2012-11-01 16:31:44 -0700

    hardcoded dataset into d3Bubble, bubble sorting now works i think

--- d3Bubble.js
+++ d3Bubble.js
@@ -1,26 +1,41 @@
-var nodes;
-var transitionDuration;
+var dataset = [[1960, 180671000, 66.6, 2.494736842, 0], [1961, 183691000, 67.1, 2.505376344, 0], [1962, 186538000, 66.9, 2.357894737, 0], [1963, 189242000, 66.6, 2.260416667, 0], [1964, 191889000, 66.8, 2.244680851, 0], 
+[1965, 194303000, 66.8, 2.063829787, 0], [1966, 196560000, 66.7, 1.936842105, 0], [1967, 198712000, 67, 1.893617021, 0], [1968, 200706000, 66, 1.795918367, 0], [1969, 202677000, 66.8, 1.884210526, 0], 
+[1970, 205052000, 67.1, 1.936842105, 0], [1971, 207661000, 67.4, 1.849462366, 0], [1972, 209896000, 67.4, 1.659574468, 0], [1973, 211909000, 67.6, 1.591397849, 0], [1974, 213854000, 68.2, 1.626373626, 0], 
+[1975, 215973000, 68.8, 1.659090909, 0], [1976, 218035000, 69.1, 1.659090909, 0], [1977, 220239000, 69.5, 1.755813953, 0], [1978, 222585000, 69.6, 1.724137931, 0], [1979, 225055000, 70, 1.835294118, 0], 
+[1980, 227225000, 70, 1.806818182, 0], [1981, 229466000, 70.3, 1.825581395, 0], [1982, 231664000, 70.8, 1.870588235, 0], [1983, 233792000, 71, 1.802325581, 0], [1984, 235825000, 71.1, 1.804597701, 0], [1985, 237924000, 71.1, 1.804597701, 0], 
+[1986, 240133000, 71.2, 1.781609195, 0], [1987, 242289000, 71.4, 1.802325581, 0], [1988, 244499000, 71.4, 1.786516854, 0], [1989, 246819000, 71.7, 1.840909091, 0], [1990, 249623000, 71.8, 1.941860465, 0], [1991, 252981000, 72, 1.895348837, 0], 
+[1992, 256514000, 72.33, 1.870588235, 0], [1993, 259919000, 72.2, 1.761363636, 0], [1994, 263126000, 72.35, 1.727272727, 0], [1995, 266278000, 72.5, 1.681818182, 0], [1996, 269394000, 73.06, 1.681818182, 0], [1997, 272657000, 73.6, 1.67816092, 0], 
+[1998, 275854000, 73.8, 1.697674419, 0], [1999, 279040000, 73.9, 1.678240741, 0], [2000, 282162411, 74.1, 1.689655172, 0], [2001, 284968955, 74.2, 1.658823529, 0], [2002, 287625193, 74.3, 1.635294118, 0], [2003, 290107933, 74.5, 1.67535545, 0], 
+[2004, 292805298, 74.9, 1.694244604, 0], [2005, 295516599, 74.9, 1.694915254, 0], [2006, 298379912, 75.1, 1.765432099, 0], [2007, 301231207, 75.4, 1.7875, 0], [2008, 304093966, 75.5, 1.744019323, 0], [2009, 306771529, 75.7, 1.642857143, 0], 
+[2010, 309349689, 75.9, 1.75, 0], [1960, 180671000, 73.1, 2.494736842, 1], [1961, 183691000, 73.6, 2.505376344, 1], [1962, 186538000, 73.5, 2.357894737, 1], [1963, 189242000, 73.4, 2.260416667, 1], [1964, 191889000, 73.7, 2.244680851, 1], 
+[1965, 194303000, 73.8, 2.063829787, 1], [1966, 196560000, 73.9, 1.936842105, 1], [1967, 198712000, 74.3, 1.893617021, 1], [1968, 200706000, 74.1, 1.795918367, 1], [1969, 202677000, 74.4, 1.884210526, 1], [1970, 205052000, 74.7, 1.936842105, 1], 
+[1971, 207661000, 75, 1.849462366, 1], [1972, 209896000, 75.1, 1.659574468, 1], [1973, 211909000, 75.3, 1.591397849, 1], [1974, 213854000, 75.9, 1.626373626, 1], [1975, 215973000, 76.6, 1.659090909, 1], [1976, 218035000, 76.8, 1.659090909, 1], 
+[1977, 220239000, 77.2, 1.755813953, 1], [1978, 222585000, 77.3, 1.724137931, 1], [1979, 225055000, 77.8, 1.835294118, 1], [1980, 227225000, 77.5, 1.806818182, 1], [1981, 229466000, 77.9, 1.825581395, 1], [1982, 231664000, 78.1, 1.870588235, 1], 
+[1983, 233792000, 78.1, 1.802325581, 1], [1984, 235825000, 78.2, 1.804597701, 1], [1985, 237924000, 78.2, 1.804597701, 1], [1986, 240133000, 78.2, 1.781609195, 1], [1987, 242289000, 78.3, 1.802325581, 1], [1988, 244499000, 78.3, 1.786516854, 1], 
+[1989, 246819000, 78.5, 1.840909091, 1], [1990, 249623000, 78.8, 1.941860465, 1], [1991, 252981000, 78.9, 1.895348837, 1], [1992, 256514000, 79.12, 1.870588235, 1], [1993, 259919000, 78.8, 1.761363636, 1], [1994, 263126000, 78.96, 1.727272727, 1], 
+[1995, 266278000, 78.9, 1.681818182, 1], [1996, 269394000, 79.08, 1.681818182, 1], [1997, 272657000, 79.4, 1.67816092, 1], [1998, 275854000, 79.5, 1.697674419, 1], [1999, 279040000, 79.4, 1.678240741, 1], [2000, 282162411, 79.3, 1.689655172, 1], 
+[2001, 284968955, 79.4, 1.658823529, 1], [2002, 287625193, 79.5, 1.635294118, 1], [2003, 290107933, 79.6, 1.67535545, 1], [2004, 292805298, 79.9, 1.694244604, 1], [2005, 295516599, 79.9, 1.694915254, 1], [2006, 298379912, 80.2, 1.765432099, 1], 
+[2007, 301231207, 80.4, 1.7875, 1], [2008, 304093966, 80.5, 1.744019323, 1], [2009, 306771529, 80.6, 1.642857143, 1], [2010, 309349689, 80.7, 1.75, 1]]; 
 
-d3.csv("dataset.csv", function(dataset) {
-//Tooltip
-tooltip = CustomTooltip("vis_tooltip", 200);
-
-//Width and height of chart
-var w = 1000;
-var h = 600;
-var padding = 50;
+       //Tooltip
+       tooltip = CustomTooltip("vis_tooltip", 200);
+       
+       //Width and height of chart
+       var w = 1000;
+       var h = 600;
+       var padding = 50;
 
 //Create scale functions
 var xScale = d3.scale.linear()
-.domain([0, d3.max(dataset, function(d) { return d.population; })])
+.domain([0, d3.max(dataset, function(d) { return d[1]; })])
 .range([padding, w - padding * 2]);
 
 var yScale = d3.scale.linear()
-.domain([0, d3.max(dataset, function(d) { return d.age; })])
+.domain([0, d3.max(dataset, function(d) { return d[2]; })])
 .range([h - padding, padding]);
 
 var rScale = d3.scale.linear()
-.domain([0, d3.max(dataset, function(d) { return d.birth_death_ratio; })])
+.domain([0, d3.max(dataset, function(d) { return d[3]; })])
 .range([20, 30]);//Change this if you want the range of the radius to differ
 
 //Define X axis
@@ -35,12 +50,11 @@ var yAxis = d3.svg.axis()
 .orient("right")
 .ticks(10);
 
-//Create SVG element
-svg = d3.select("#vis")
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h);
-
+       //Create SVG element
+       var svg = d3.select("#vis")
+       .append("svg")
+       .attr("width", w)
+       .attr("height", h);
 
 //Initial Transition Duration and Gender Switch Duration
 var transitionDuration = 1000;
@@ -54,21 +68,21 @@ var gnodes = svg.selectAll(".node").data(dataset)
          .enter().append("g")
          .each(generateCirclesAndText)
          .attr("class", "node")
-         .attr("transform",function(d) {return "translate("+xScale(d.population) +",600)"})
+         .attr("transform",function(d) {return "translate("+xScale(d[1]) +",600)"})
          .transition()//This transition is pretty useless just experimenting. It fades and rises the circles a little on load. 
          .duration(transitionDuration)
           .style('opacity', .75)
-          .attr("transform",function(d) {return "translate("+xScale(d.population) +"," + yScale(d.age)+")"})
+          .attr("transform",function(d) {return "translate("+xScale(d[1]) +"," + yScale(d[2])+")"})
 
 //Use this selector to manipulate the bubbles
-nodes = svg.selectAll(".node").data(dataset);
+var nodes = svg.selectAll(".node").data(dataset);
 
 //draws circle and text within gnode
 function generateCirclesAndText(d){
   //gnode is this
   d3.select(this).append("circle")
       .attr("r", function(d) {
-        return rScale(d.birth_death_ratio);
+        return rScale(d[3]);
       })
       .attr("cx", function(d) {
         return 0;
@@ -77,7 +91,7 @@ function generateCirclesAndText(d){
         return 0;//0 to rise up from the axis
       })
       .attr("fill",function(d) {
-        if(d.gender == 0)
+        if(d[4] == 0)
           return "steelblue";
         else
           return "pink";
@@ -85,7 +99,7 @@ function generateCirclesAndText(d){
       .attr("stroke","black");
   d3.select(this).append("text")
       .attr("text-anchor","middle")
-      .text(function(d) { return d.year; });
+      .text(function(d) { return d[0]; });
 }
 
 
@@ -101,10 +115,10 @@ nodes.on('mouseover', function(d, i) {
    return d == p;
   })
   .each(function(selection){
-    console.log(selection.year)//gives the node at the first array position
-    var content = "<span class=\"name\">Population in "+selection.year+":</span><span class=\"value\"> " + selection.population + "</span><br/>";
-    content += "<span class=\"name\">You'll live to age</span><span class=\"value\"> " + selection.age + "</span><br/>";
-    content += "<span class=\"name\">Ratio of births/deaths in "+selection.year+":</span><span class=\"value\"> " + selection.birth_death_ratio + "</span><br/>";
+    console.log(selection[0])//gives the node at the first array position
+    var content = "<span class=\"name\">Population in "+selection[0]+":</span><span class=\"value\"> " + selection[1] + "</span><br/>";
+    content += "<span class=\"name\">You'll live to age</span><span class=\"value\"> " + selection[2] + "</span><br/>";
+    content += "<span class=\"name\">Ratio of births/deaths in "+selection[0]+":</span><span class=\"value\"> " + selection[3] + "</span><br/>";
     return tooltip.showTooltip(content, d3.event)//need event capture, typically content, event, selection 
   });
 })
@@ -149,28 +163,28 @@ svg.append("text")
     .attr("dy", ".75em")
     .attr("transform", "rotate(-90)")
     .text("Life Expectancy (years)");
-});
+       
 
 //When the user clicks to filter by gender
-
 function toggle_view(gender){
   nodes
     .transition()
     .duration(transitionDuration)
      .style('opacity', '.75');
   var type;
+      
   if(gender=="male")
     type = 0;
   if(gender=="female")
     type = 1;
   if(gender!="both"){
-    nodes.filter(function(p) {
-       return type != p.gender;
+    nodes.filter(function(d) {
+       return type != d[4];
       })
       .transition()
       .duration(transitionDuration)
-       .style('opacity', '.1');
-    }
+      .style('opacity', '.1');
+  }
 }
 
 
